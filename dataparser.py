92bf2fb8629eb5a1e73bbae9ddcb1516fa755661
Your Name <shananalla88@gmail.com>
2020-09-10 20:35:32 +0630

    auto

--- dataparser.py
+++ dataparser.py
@@ -23,10 +23,6 @@ state_code_to_name={'pb':'Punjab',            'hr':'Haryana',
 state_name_to_code={}
 for k in state_code_to_name: state_name_to_code[state_code_to_name[k]]=k
 
-#cache this to avoid repeated file reads
-global_karnataka_case_series=get_cases(state='Karnataka',case_type='confirmed',return_full_series=True,verbose=False)
-
-
 def helper_download_karnataka_bulletin(twitter_link):
   x=requests.get(twitter_link)
   url=x.url
@@ -521,6 +517,10 @@ def make_plots(use_all_states=False,use_solid_lines=False):
   pylab.title('Ventilator use in states over time')
   pylab.show(); pylab.close()
   figure = pylab.gcf();figure.set_size_inches(8, 6);pylab.savefig('Ventilator_usage.png', dpi = 100);
+
+#cache this to avoid repeated file reads
+global_karnataka_case_series=get_cases(state='Karnataka',case_type='confirmed',return_full_series=True,verbose=False)
+
     
 if __name__=='__main__':
   make_plots()
