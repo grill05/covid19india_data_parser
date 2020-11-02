bfcf778acb2784ec46b13ec9d3c572d7b27e4393
Your Name <you@example.com>
2020-11-02 19:42:12 +0630

    auto

--- delhiparse.py
+++ delhiparse.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import os,sys,bs4
 
-def parse():
-    cmd='wget https://delhifightscorona.in/data/non-covid-icu-beds/ -O tmp.htm';os.system(cmd)
+def parse(vent=False):
+    website='https://delhifightscorona.in/data/non-covid-icu-beds/'
+    if vent: website='https://delhifightscorona.in/data/ventilators/'
+    cmd='wget "'+website+'" -O tmp.htm';os.system(cmd)
     soup=bs4.BeautifulSoup('tmp.htm')
     x=soup('div',attrs={'class':'callout'})
     total=int(x[0]('h1')[0].text)
@@ -10,5 +12,7 @@ def parse():
     vacant=int(x[2]('h1')[0].text)
     date=datetime.datetime.now().strftime('%d-%m')
     print('In Delhi, on date: %s, non-covid icu beds:\ntotal: %d\noccupied: %d\nvacant: %d' %(date,total,occupied,vacant))
+    os.remove('tmp.htm')
 if __name__=='__main__':
     parse()
+    parse(vent=True)
